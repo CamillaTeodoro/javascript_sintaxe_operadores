d595eae4e4c7baf2a52a7a314408dfb2567dd490
Camilla <camillateodoro@hotmail.com>
2022-01-07 15:02:47 -0300

    primeira versão do código

---
+++ script.js
@@ -0,0 +1,30 @@
+let n1 = Number(prompt('Insira o primeiro valor:'));
+let n2 = Number(prompt('Insira o segundo valor:'));
+let resultado;
+
+
+
+function soma() {
+    resultado = n1 + n2;
+    alert(`${n1} + ${n2} = ${resultado}`)
+
+}
+
+function compara() {
+
+    if (resultado >= 10 && resultado <= 20) {
+        alert(`${resultado} é maior que 10 e menor que 20`)
+    } else {
+        alert(`${resultado} é menor que 10 ou maior que 20`)
+    }
+
+}
+
+if (n1 === n2) {
+    alert(`${n1} é igual a ${n2}`)
+} else {
+    alert(`${n1} é diferente de ${n2}`);
+}
+
+soma();
+compara();
